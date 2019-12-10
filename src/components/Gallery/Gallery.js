2ed44123fddd6f53ea9d2d9cd60c45f8a1da0a05
Gal Shitrit <gal@shitrit.net>
2019-12-10 16:53:59 +0200

    Stage 2.2 Complete without UI

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -24,12 +24,13 @@ class Gallery extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      favorites: [],
       images: [],
       loading: false,
       page: 1,
       galleryWidth: this.getGalleryWidth(),
       modalIsOpen: false,
-      modalImage: ''
+      modalImage: ""
     };
     this.openModal = this.openModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
@@ -72,34 +73,37 @@ class Gallery extends React.Component {
   }
 
   loadMore() {
-    this.setState({ loading: true, page: this.state.page +1 });
-    debugger;
-    setTimeout(() => {
-      const getImagesUrl = `services/rest/?method=flickr.photos.search&api_key=522c1f9009ca3609bcbaf08545f067ad&tags=${this.props.tag}&tag_mode=any&per_page=100&format=json&safe_search=1&nojsoncallback=1&page=${this.state.page}`;
-      // eslint-disable-next-line quotes
-      const baseUrl = "https://api.flickr.com/";
-      axios({
-        url: getImagesUrl,
-        baseURL: baseUrl,
-        method: 'GET'
-      })
-        .then(res => res.data)
-        .then(res => {
-          if (
-            res &&
-            res.photos &&
-            res.photos.photo &&
-            res.photos.photo.length > 0
-          ) {
-            let newImages = this.state.images.concat(res.photos.photo);
-            this.setState({ images: newImages });
-          }
-        });
-    }, 2000);
+    this.setState({ loading: true, page: this.state.page + 1 });
+    const getImagesUrl = `services/rest/?method=flickr.photos.search&api_key=522c1f9009ca3609bcbaf08545f067ad&tags=${this.props.tag}&tag_mode=any&per_page=100&format=json&safe_search=1&nojsoncallback=1&page=${this.state.page}`;
+    // eslint-disable-next-line quotes
+    const baseUrl = "https://api.flickr.com/";
+    axios({
+      url: getImagesUrl,
+      baseURL: baseUrl,
+      method: "GET"
+    })
+      .then(res => res.data)
+      .then(res => {
+        if (
+          res &&
+          res.photos &&
+          res.photos.photo &&
+          res.photos.photo.length > 0
+        ) {
+          let newImages = this.state.images.concat(res.photos.photo);
+          this.setState({ images: newImages });
+        }
+      });
   }
 
   componentDidMount() {
     this.getImages(this.props.tag, this.state.page);
+    let localFavs =  JSON.parse(localStorage.getItem('favorites'));
+    localFavs === null
+      ? null
+      : this.setState({
+          favorites: localFavs
+        });
     this.setState({
       galleryWidth: document.body.clientWidth
     });
@@ -108,20 +112,29 @@ class Gallery extends React.Component {
   componentWillReceiveProps(props) {
     this.getImages(props.tag, this.state.page);
   }
-  handleScroll = (e) => {
-    debugger;
-    const bottom = e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
-    if (bottom) { this.loadMore() }
-  }
+  handleScroll = e => {
+    const bottom =
+      e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
+    if (bottom) {
+      this.loadMore();
+    }
+  };
   clone(img) {
     let prevState = this.state.images;
     prevState.splice(0, 0, img);
     this.setState({ images: prevState });
   }
+  onFavorite() {
+    const img = this.state.modalImage;
+    const currFavorites = this.state.favorites;
+    !this.state.favorites.includes(img)? currFavorites.push(img) : null;
+    this.setState({ ...currFavorites });
+    localStorage.setItem("favorites", JSON.stringify(this.state.favorites));
+  }
 
   render() {
     return (
-      <div className="gallery-root" onScroll={this.handleScroll}>
+      <div className="gallery-root" onScroll={this.handleScroll.bind(this)}>
         <Modal
           appElement={document.getElementById("app")}
           isOpen={this.state.modalIsOpen}
@@ -130,7 +143,9 @@ class Gallery extends React.Component {
           ariaHideApp={false}
           contentLabel="Example Modal"
         >
-          <img src={this.state.modalImage}
+          <img
+            src={this.state.modalImage}
+            onDoubleClick={this.onFavorite.bind(this)}
           ></img>
         </Modal>
         {this.state.images.map((dto, index) => {
@@ -144,11 +159,18 @@ class Gallery extends React.Component {
             />
           );
         })}
-        {this.state.loading
-	      ? <p className="App-intro">
-		  loading ...
-		</p>
-	      : ""}
+        {this.state.loading ? (
+          <div className="sk-chase">
+            <div className="sk-chase-dot"></div>
+            <div className="sk-chase-dot"></div>
+            <div className="sk-chase-dot"></div>
+            <div className="sk-chase-dot"></div>
+            <div className="sk-chase-dot"></div>
+            <div className="sk-chase-dot"></div>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     );
   }
